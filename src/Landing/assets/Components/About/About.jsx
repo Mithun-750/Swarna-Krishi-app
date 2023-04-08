b4fb876fb99b2ddf4645d5b4810aa9431af5e0d4
Mithun.U <www.mightymanme@gmail.com>
2023-04-08 17:10:40 +0530

    Update About.jsx

--- src/Landing/assets/Components/About/About.jsx
+++ src/Landing/assets/Components/About/About.jsx
@@ -8,7 +8,7 @@ export class About extends Component {
                     {/* <img className="d-block mx-auto mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" /> */}
                     <h1 className="display-5 fw-bold">About</h1>
                     <div className="col-lg-6 mx-auto">
-                        <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world's most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                        <p className="lead mb-4">Simple Farm managing tool which helps a farmers keep track if fertilization and irrigation intervals and also help them keep track of the weather. This tool also helps them by recommending the best possibble crop that can be grown on the Land. </p>
                         <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
                             <button type="button" className="btn btn-primary btn-lg px-4 gap-3" fdprocessedid="y86du">Donate Us</button>
                             <button type="button" className="btn btn-outline-secondary btn-lg px-4" fdprocessedid="kyd7k">Source code</button>
