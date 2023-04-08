71f2047cc93bac97c335222eff59a54818d44b0d
Mithun.U <www.mightymanme@gmail.com>
2023-04-08 17:13:04 +0530

    title

--- src/Landing/assets/Components/About/About.jsx
+++ src/Landing/assets/Components/About/About.jsx
@@ -11,7 +11,9 @@ export class About extends Component {
                         <p className="lead mb-4">Simple Farm managing tool which helps a farmers keep track if fertilization and irrigation intervals and also help them keep track of the weather. This tool also helps them by recommending the best possibble crop that can be grown on the Land. </p>
                         <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
                             <button type="button" className="btn btn-primary btn-lg px-4 gap-3" fdprocessedid="y86du">Donate Us</button>
-                            <button type="button" className="btn btn-outline-secondary btn-lg px-4" fdprocessedid="kyd7k">Source code</button>
+                            <a href="https://github.com/Mithun-750/Swarna-Krishi-app" target="_blank" rel="noopener noreferrer">
+                                <button type="button" className="btn btn-outline-secondary btn-lg px-4" fdprocessedid="kyd7k">Source code</button>
+                            </a>
                         </div>
                     </div>
                 </section>
